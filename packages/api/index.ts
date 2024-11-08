23bdcc47888a609cda2b69faedc0aab907abca12
Jon Wire <wirej@amazon.com>
2024-11-08 14:52:55 -0600

    fixed build

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,10 +1,14 @@
+import { writeFileSync } from "fs";
+
 const stuff = [] as number[];
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 10; i++) {
   stuff.push(i);
 }
 
-console.log(`
+const dist = `
   const stuff = ${JSON.stringify(stuff, null, 2)} as const;
   export { stuff };
-`);
+`;
+
+writeFileSync("dist.ts", dist);
