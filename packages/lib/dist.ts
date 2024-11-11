87132a991a26ff5e1986b7812a9a01933f0d2168
Jon Wire <wirej@amazon.com>
2024-11-11 15:31:01 -0600

    a little reorganizing, prepping for multi-api testing

--- packages/lib/dist.ts
+++ packages/lib/dist.ts
@@ -1,4 +1,4 @@
-const stuff = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] as const;
+const stuff = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] as const;
 const things = {
   a: "something",
   y: "something else",
--- packages/main/index.ts
+++ packages/main/index.ts
@@ -1,4 +1,4 @@
-import { stuff, things } from "generate-api";
+import { stuff, things } from "lib";
 
 // just here to comment and uncomment to quickly retest build.
 // console.log("ok");
